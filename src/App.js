15477f42538b5ad109462c2db91ec8dc1f2ae12a
lukatassano <lukatassano070@gmail.com>
2020-05-09 16:18:43 -0300

    edit css

--- src/App.js
+++ src/App.js
@@ -4,38 +4,35 @@ import serverCep from 'cep-promise';
 
 function App() {
   const [cep, setCep] = useState('');
-  const [state, setState] = useState();
-  const [city, setCity] = useState();
-  const [neighborhood, setNeighborhood] = useState();
-  const [street, setStreet] = useState();
+  const [responseCep, setResponseCep] = useState();
 
 
-  async function handleVerify() {
-    const verify = await serverCep(cep);
-    const {city, street, state, neighborhood} = verify;
-    console.log(verify);
-    setCity(city);
-    setStreet(street);
-    setState(state);
-    setNeighborhood(neighborhood);
+  async function handleVerify(cep) {
+    try {
+      const response = serverCep(cep)
+        .then(function () {
+          setResponseCep(response)
+        })
+    } catch (err) {
+      alert(err)
+    }
     return
   }
 
   return (
-    <Container className="container">
-      <div className="input-div">
-        <input className="input-cep" placeholder="CEP" onChange={event => setCep(event.target.value)}/>
-        <button className="btn" onClick={() => handleVerify()}>
-          Verificar
-        </button>
-      </div>
-      <div className="response">
-        <p>{state}</p>
-        <p>{city}</p>
-        <p>{neighborhood}</p>
-        <p>{street}</p>
-      </div>
-    </Container>
+    <>
+      <Container className="container">
+        <div className="input-div">
+          <input className="input-cep" placeholder="CEP" onChange={event => setCep(event.target.value)}/>
+          <button className="btn" onClick={() => handleVerify()}>
+            Buscar
+          </button>
+        </div>
+        <div className="response">
+          <h1>{responseCep}</h1>
+        </div>
+      </Container>
+    </>
   );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -1,13 +1,21 @@
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
+import satellite from './assets/satellite.svg'
 
 export const Container = styled.div`
   display: flex;
   height: 60vh;
+  background-color: rgba(255,255,255, 0.45);
   border-radius: 20px;
   align-items: center;
   justify-content: center;
 
+  img {
+    height: 100px;
+  }
+
   .input-div{
+    background-color: #fff;
+    border-radius: 20px;
     display: flex;
     align-self: center;
     align-items: center;
@@ -15,41 +23,55 @@ export const Container = styled.div`
   }
 
   input {
-    border-top-left-radius: 10px;
-    border-bottom-left-radius: 10px;
+    border-top-left-radius: 25px;
+    border-bottom-left-radius: 25px;
+    background-color: rgba(255, 255, 255, 0.8);
     border: 0px;
     height: 40px;
-    width: 60%;
+    width: 300px;
     font-size: 22px;
     text-align: center;
   }
 
 
   .btn {
+    width: 100px;
+    background-color: rgba(255, 0, 0, 0.8);
     font-weight: bold;
-    border: 0px;
-    padding: 0 20px 0 20px;
     font-size: 15px;
     color: #fff;
-    background-color: #7159c1;
     height: 40px;
-    border-top-right-radius: 10px;
-    border-bottom-right-radius: 10px;
+    border: 0px;
+    border-radius: 20px;
     cursor: pointer;
     transition: opacity 0.2s;
 
     &:hover{
-      opacity: 80%;
+      opacity: 70%;
     }
   }
 
   .response {
-    margin-top: 80px;
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 20px;
+    height: 200px;
+    width: 400px;
+    background-color: rgba(0, 0, 0, 0.4);
+    align-items: center;
+    justify-content: center;
+    transition: width 0.2s;
+
+    &:hover{
+      width: 420px;
+    }
   }
 
-
   p {
+    color: #fff;
     font-family: Arial, Helvetica, sans-serif;
     font-size: 20px;
+    font-weight: bold;
   }
 `
